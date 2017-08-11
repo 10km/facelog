811ed1e09374d4c4f6d5c88ea27375b9282e4f77
10km <10km0811@sohu.com>
2017-08-12 00:02:00 +0800

    driver.lib 和 driver.jar 分别改名为 driver-lib 和 driver-jar

--- db/generate.bat
+++ db/generate.bat
@@ -1,5 +1,5 @@
 pushd sql2java
 rem ant rebuild -Dsql2java-config=../sql2java.properties -Dgenerated-src=../src/main/java
-ant rebuild -Dsql2java-config=../sql2java.properties -Dgenerated-src=../src/main/java -Ddriver.lib=../lib 
-rem ant rebuild -Dsql2java-config=../sql2java.properties -Dgenerated-src=../src/main/java -Ddriver.jar=../lib/mysql-connector-java-5.1.43-bin.jar
+ant rebuild -Dsql2java-config=../sql2java.properties -Dgenerated-src=../src/main/java -Ddriver-lib=../lib 
+rem ant rebuild -Dsql2java-config=../sql2java.properties -Dgenerated-src=../src/main/java -Ddriver-jar=../lib/mysql-connector-java-5.1.43-bin.jar
 popd
