8bf4b74d218df660c05913fef0efdf64b7ea990b
10km <10km0811@sohu.com>
2017-08-11 18:39:26 +0800

    增加 driver.lib driver.jar 变量,允许 外部定义数据库驱动程序

--- db/generate.bat
+++ db/generate.bat
@@ -1,3 +1,5 @@
 pushd sql2java
-ant rebuild -Dsql2java-config=..\sql2java.properties -Dgenerated-src=..\src\main\java
+rem ant rebuild -Dsql2java-config=../sql2java.properties -Dgenerated-src=../src/main/java
+ant rebuild -Dsql2java-config=../sql2java.properties -Dgenerated-src=../src/main/java -Ddriver.lib=../lib 
+rem ant rebuild -Dsql2java-config=../sql2java.properties -Dgenerated-src=../src/main/java -Ddriver.jar=../lib/mysql-connector-java-5.1.43-bin.jar
 popd
