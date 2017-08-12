ebf6f6f0fa4f770d0a1d0afdb246ca315d4a8ffa
10km <10km0811@sohu.com>
2017-08-12 18:10:30 +0800

    改为maven调用

--- db/generate.bat
+++ db/generate.bat
@@ -1,5 +1,6 @@
-pushd sql2java
+cd sql2java
 rem ant rebuild -Dsql2java-config=../sql2java.properties -Dgenerated-src=../src/main/java
-ant rebuild -Dsql2java-config=../sql2java.properties -Dgenerated-src=../src/main/java -Ddriver-lib=../lib 
+rem ant rebuild -Dsql2java-config=../sql2java.properties -Dgenerated-src=../src/main/java -Ddriver-lib=../lib 
 rem ant rebuild -Dsql2java-config=../sql2java.properties -Dgenerated-src=../src/main/java -Ddriver-jar=../lib/mysql-connector-java-5.1.43-bin.jar
-popd
+mvn  generate-sources -Dsql2java-config=../sql2java.properties -Dgenerated-src=../src/main/java -Ddriver-jar=../lib/mysql-connector-java-5.1.43-bin.jar
+cd ..
