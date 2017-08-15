145cd46c97d5415805d7f620fa6be36132a32354
10km <10km0811@sohu.com>
2017-08-15 10:39:06 +0800

    db内容移到orm,sql2java 改为 submodule 管理

--- db/generate.bat
+++
@@ -1,6 +0,0 @@
-cd sql2java
-rem ant rebuild -Dsql2java-config=../sql2java.properties -Dgenerated-src=../src/main/java
-rem ant rebuild -Dsql2java-config=../sql2java.properties -Dgenerated-src=../src/main/java -Ddriver-lib=../lib 
-rem ant rebuild -Dsql2java-config=../sql2java.properties -Dgenerated-src=../src/main/java -Ddriver-jar=../lib/mysql-connector-java-5.1.43-bin.jar
-mvn antrun:run -Dsql2java-config=../sql2java.properties -Dgenerated-src=../src/main/java -Ddriver-jar=../lib/mysql-connector-java-5.1.43-bin.jar
-cd ..
