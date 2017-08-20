92bd643e75aa12286cb40f3f9c3e72aa66bfe6a5
10km <10km0811@sohu.com>
2017-08-20 23:35:32 +0800

    增 ByteBuffer 转换代码,完善说明

--- db/src/test/java/net/gdface/facelog/TestSerialVersionUID.java
+++ db/src/test/java/net/gdface/facelog/TestSerialVersionUID.java
@@ -3,6 +3,8 @@ package net.gdface.facelog;
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import org.junit.Test;
@@ -65,19 +67,41 @@ public class TestSerialVersionUID {
 		byte[] md5 = getMD5(input.getBytes());
 		return longFrom8Bytes(md5,0, false)  ^ longFrom8Bytes(md5,8, false);
 	}
+    /**
+     * 利用 {@link java.nio.ByteBuffer}实现byte[]转long
+     * @param input
+     * @param offset 
+     * @param littleEndian 输入数组是否小端模式
+     * @return
+     */
+    public static long bytesToLong(byte[] input, int offset, boolean littleEndian) { 
+		if(offset <0 || offset+8>input.length)
+			throw new IllegalArgumentException(String.format("less than 8 bytes from index %d  is insufficient for long",offset));
+    	ByteBuffer buffer = ByteBuffer.wrap(input,offset,8);
+    	if(littleEndian){
+        	// ByteBuffer.order(ByteOrder) 方法指定字节序,即大小端模式(BIG_ENDIAN/LITTLE_ENDIAN)
+        	// ByteBuffer 默认为大端(BIG_ENDIAN)模式 
+        	buffer.order(ByteOrder.LITTLE_ENDIAN);
+    	}
+        return buffer.getLong();  
+    } 
 	@Test
 	public void test() throws IOException {
 		String input="net.gdface.facelog.dborm.person.FlPersonBeanBase";
 		byte[] md5 = getMD5(input.getBytes());
-		System.out.println(toHex(md5));
-		ByteArrayInputStream byteInput = new ByteArrayInputStream(md5);
-		DataInputStream dataInput = new DataInputStream(byteInput);	
+		System.out.printf("md5 [%s]\n",toHex(md5));
+		// 三种方式运算结果对比验证
+		DataInputStream dataInput = new DataInputStream(new ByteArrayInputStream(md5));	
 		long l1 = dataInput.readLong();
 		long l2 = dataInput.readLong();
-		System.out.printf("l1=0x%x l2=0x%x\n", l1,l2);
-		long l3 = l1 ^ l2;
+		System.out.printf("l1=0x%x l2=0x%x,DataInputStream\n", l1,l2);
+		System.out.printf("%x ,DataInputStream\n",l1 ^ l2);
 		
-		System.out.println(l3);
+		long ln1 = bytesToLong(md5,0, false);
+		long ln2 = bytesToLong(md5,8, false);
+		System.out.printf("ln1=0x%x ln2=0x%x,ByteBuffer\n", ln1,ln2);
+		System.out.printf("%x ,ByteBuffer\n",ln1 ^ ln2);
+		
 		long ll1 = longFrom8Bytes(md5,0, false);
 		long ll2 = longFrom8Bytes(md5,8, false);	
 		System.out.printf("ll1=0x%x ll2=0x%x\n", ll1,ll2);
