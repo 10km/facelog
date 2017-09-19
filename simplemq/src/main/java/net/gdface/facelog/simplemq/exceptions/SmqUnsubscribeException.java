c5c9afbf8eb39d8e82ff00f16536d4a5cedffc74
10km <10km0811@sohu.com>
2017-09-19 16:49:39 +0800

    commit

---
+++ simplemq/src/main/java/net/gdface/facelog/simplemq/exceptions/SmqUnsubscribeException.java
@@ -0,0 +1,14 @@
+package net.gdface.facelog.simplemq.exceptions;
+
+import net.gdface.facelog.simplemq.ChannelDispatcher;
+import net.gdface.facelog.simplemq.IMessageAdapter;
+import net.gdface.facelog.simplemq.IMessageDispatcher;
+
+/**
+ * 取消订阅异常, {@link IMessageAdapter}接口抛出该异常时,{@link IMessageDispatcher}对象会取消订阅当前频道<br>
+ * 参见 {@link ChannelDispatcher#dispatch(String, String)} 实现代码
+ * @author guyadong
+ *
+ */
+public class SmqUnsubscribeException extends SmqExcepiton{
+	private static final long serialVersionUID = 1L;}
