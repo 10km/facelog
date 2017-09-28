7227222834af032922609714535fcf38405ec340
10km <10km0811@sohu.com>
2017-09-28 18:04:15 +0800

    commit

--- facelog-main/src/main/java/net/gdface/facelog/FaceLogDbLocal.java
+++ facelog-main/src/main/java/net/gdface/facelog/FaceLogDbLocal.java
@@ -1,5 +1,8 @@
 package net.gdface.facelog;
 
+import java.util.Collection;
+import java.util.List;
+
 import net.gdface.facelog.db.DeviceBean;
 import net.gdface.facelog.db.FaceBean;
 import net.gdface.facelog.db.FeatureBean;
@@ -20,6 +23,30 @@ public class FaceLogDbLocal implements FaceLogDb,CommonConstant,
 	private static TableManager<PersonBean> personManager = TableInstance.getInstance(PersonBean.class);
 	private static TableManager<StoreBean> storeManager = TableInstance.getInstance(StoreBean.class);
 	public FaceLogDbLocal() {
+	}
+	public PersonBean getPerson(int id) {
+		return personManager.loadByPrimaryKey(id);
+	}
+	public int deletePerson(int id) {
+		return personManager.deleteByPrimaryKey(id);
+	}
+	public boolean existsPerson(int id) {
+		return personManager.existsPrimaryKey(id);
+	}
+	public PersonBean savePerson(PersonBean bean) {
+		return personManager.save(bean);
+	}	
+	public void savePerson(List<PersonBean> beans) {
+		personManager.saveAsTransaction(beans);
+	}
+	
+	public PersonBean getPersonByPapersNum(String papersNum) {
+		PersonBean bean = new PersonBean();
+		bean.setPapersNum(papersNum);
+		return personManager.loadUniqueUsingTemplate(bean);
+	}
+	public PersonBean savePerson(PersonBean bean, ImageBean refImage,FaceBean[] includeFaces) {
+		return personManager.saveAsTransaction(bean, refImage,includeFaces);
 	}
 
 }
