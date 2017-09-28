ea8eb300ce21fd1a589fdc8d28c154e9f259c878
10km <10km0811@sohu.com>
2017-09-28 09:41:53 +0800

    commit

--- facelog-main/src/main/java/net/gdface/facelog/FaceLogDbLocal.java
+++ facelog-main/src/main/java/net/gdface/facelog/FaceLogDbLocal.java
@@ -8,23 +8,17 @@ import net.gdface.facelog.db.LogBean;
 import net.gdface.facelog.db.PersonBean;
 import net.gdface.facelog.db.StoreBean;
 import net.gdface.facelog.db.TableManager;
-import net.gdface.facelog.db.mysql.DeviceManager;
-import net.gdface.facelog.db.mysql.FaceManager;
-import net.gdface.facelog.db.mysql.FeatureManager;
-import net.gdface.facelog.db.mysql.ImageManager;
-import net.gdface.facelog.db.mysql.LogManager;
-import net.gdface.facelog.db.mysql.PersonManager;
-import net.gdface.facelog.db.mysql.StoreManager;
+import net.gdface.facelog.db.mysql.TableInstance;
 
 public class FaceLogDbLocal implements FaceLogDb,CommonConstant,
 		net.gdface.facelog.db.Constant {
-	private static TableManager<DeviceBean> deviceManager = DeviceManager.getInstance();
-	private static TableManager<FaceBean> faceManager = FaceManager.getInstance();
-	private static TableManager<FeatureBean> featureManager = FeatureManager.getInstance();
-	private static TableManager<ImageBean> imagemanager = ImageManager.getInstance();
-	private static TableManager<LogBean> logManager = LogManager.getInstance();
-	private static TableManager<PersonBean> personManager = PersonManager.getInstance();
-	private static TableManager<StoreBean> storeManager = StoreManager.getInstance();
+	private static TableManager<DeviceBean> deviceManager = TableInstance.getInstance(DeviceBean.class);
+	private static TableManager<FaceBean> faceManager = TableInstance.getInstance(FaceBean.class);
+	private static TableManager<FeatureBean> featureManager = TableInstance.getInstance(FeatureBean.class);
+	private static TableManager<ImageBean> imagemanager = TableInstance.getInstance(ImageBean.class);
+	private static TableManager<LogBean> logManager = TableInstance.getInstance(LogBean.class);
+	private static TableManager<PersonBean> personManager = TableInstance.getInstance(PersonBean.class);
+	private static TableManager<StoreBean> storeManager = TableInstance.getInstance(StoreBean.class);
 	public FaceLogDbLocal() {
 	}
 
