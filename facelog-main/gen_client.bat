40a0d879a29b4a8fc6010aeedc6f3d300cd754c5
10km <10km0811@sohu.com>
2017-10-12 09:09:24 +0800

    commit

--- facelog-main/gen_client.bat
+++ facelog-main/gen_client.bat
@@ -1,6 +1,8 @@
+set OUT_FOLDER=..\facelog-client\src\thrift\java
+rmdir  %OUT_FOLDER% /s/q
 java -jar lib\swift-generator-cli-0.14.2-standalone.jar ^
 	IFaceLog.thrift ^
 	-generate_beans ^
-	-override_package net.gdface.facelog.client ^
-	-out ../facelog-client/src/main/java
+	-override_package net.gdface.facelog.client.thrift ^
+	-out %OUT_FOLDER%
  	
