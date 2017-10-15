433c98c7a81823dffd0b2df77ef8c153b549d3a9
10km <10km0811@sohu.com>
2017-10-15 18:37:09 +0800

    commit

--- facelog-client/src/sql2java/java/net/gdface/facelog/client/IFaceLogClient.java
+++ facelog-client/src/sql2java/java/net/gdface/facelog/client/IFaceLogClient.java
@@ -16,105 +16,149 @@ IFaceLog
 	param 0: feature class java.nio.ByteBuffer
 	param 1: personId class java.lang.Integer
 	param 2: faecBeans java.util.List<net.gdface.facelog.db.FaceBean>
+	return:class net.gdface.facelog.db.FeatureBean
 1 name: addFeatureMulti original name: addFeature 
 	param 0: feature class java.nio.ByteBuffer
 	param 1: personId class java.lang.Integer
 	param 2: faceInfo java.util.Map<java.nio.ByteBuffer, net.gdface.facelog.db.FaceBean>
 	param 3: deviceId class java.lang.Integer
+	return:class net.gdface.facelog.db.FeatureBean
 2 name: addImage 
 	param 0: imageData class java.nio.ByteBuffer
 	param 1: deviceId class java.lang.Integer
 	param 2: faceBean class net.gdface.facelog.db.FaceBean
 	param 3: personId class java.lang.Integer
+	return:class net.gdface.facelog.db.ImageBean
 3 name: addLog 
 	param 0: bean class net.gdface.facelog.db.LogBean
+	return:void
 4 name: addLogList original name: addLog 
 	param 0: beans java.util.List<net.gdface.facelog.db.LogBean>
+	return:void
 5 name: countLogLightWhere 
 	param 0: where class java.lang.String
+	return:int
 6 name: countLogWhere 
 	param 0: where class java.lang.String
+	return:int
 7 name: deleteAllFeaturesByPersonId 
 	param 0: personId int
 	param 1: deleteImage boolean
+	return:int
 8 name: deleteFeature 
 	param 0: featureMd5 class java.lang.String
 	param 1: deleteImage boolean
+	return:java.util.List<java.lang.String>
 9 name: deleteImage 
 	param 0: imageMd5 class java.lang.String
+	return:int
 10 name: deletePerson 
 	param 0: personId int
+	return:int
 11 name: deletePersonByPapersNum 
 	param 0: papersNum class java.lang.String
+	return:int
 12 name: deletePersons 
 	param 0: personIdList java.util.List<java.lang.Integer>
+	return:int
 13 name: deletePersonsByPapersNum 
 	param 0: papersNumlist java.util.List<java.lang.String>
+	return:int
 14 name: disablePerson 
 	param 0: personId int
+	return:void
 15 name: disablePersonList original name: disablePerson 
 	param 0: personIdList java.util.List<java.lang.Integer>
+	return:void
 16 name: existsDevice 
 	param 0: id int
+	return:boolean
 17 name: existsFeature 
 	param 0: md5 class java.lang.String
+	return:boolean
 18 name: existsImage 
 	param 0: md5 class java.lang.String
+	return:boolean
 19 name: existsPerson 
 	param 0: persionId int
+	return:boolean
 20 name: getDevice 
 	param 0: deviceId class java.lang.Integer
+	return:class net.gdface.facelog.db.DeviceBean
 21 name: getDeviceList original name: getDevice 
 	param 0: deviceId java.util.List<java.lang.Integer>
+	return:java.util.List<net.gdface.facelog.db.DeviceBean>
 22 name: getFeature 
 	param 0: md5 class java.lang.String
+	return:class net.gdface.facelog.db.FeatureBean
 23 name: getFeatureBeansByPersonId 
 	param 0: personId int
+	return:java.util.List<java.lang.String>
 24 name: getFeatureBytes 
 	param 0: md5 class java.lang.String
+	return:class java.nio.ByteBuffer
 25 name: getFeatureList original name: getFeature 
 	param 0: md5 java.util.List<java.lang.String>
+	return:java.util.List<net.gdface.facelog.db.FeatureBean>
 26 name: getImage 
 	param 0: imageMD5 class java.lang.String
+	return:class net.gdface.facelog.db.ImageBean
 27 name: getImageBytes 
 	param 0: imageMD5 class java.lang.String
+	return:class java.nio.ByteBuffer
 28 name: getImagesAssociatedByFeature 
 	param 0: featureMd5 class java.lang.String
+	return:java.util.List<java.lang.String>
 29 name: getLogBeansByPersonId 
 	param 0: personId int
+	return:java.util.List<net.gdface.facelog.db.LogBean>
 30 name: getPerson 
 	param 0: personId int
+	return:class net.gdface.facelog.db.PersonBean
 31 name: getPersonByPapersNum 
 	param 0: papersNum class java.lang.String
+	return:class net.gdface.facelog.db.PersonBean
 32 name: getPersons 
 	param 0: idList java.util.List<java.lang.Integer>
+	return:java.util.List<net.gdface.facelog.db.PersonBean>
 33 name: isDisable 
 	param 0: personId int
+	return:boolean
 34 name: loadAllPerson 
+	return:java.util.List<java.lang.Integer>
 35 name: loadFeatureMd5ByUpdate 
 	param 0: timestamp long
+	return:java.util.List<java.lang.String>
 36 name: loadLogByWhere 
 	param 0: where class java.lang.String
 	param 1: startRow int
 	param 2: numRows int
+	return:java.util.List<net.gdface.facelog.db.LogBean>
 37 name: loadLogLightByWhere 
 	param 0: where class java.lang.String
 	param 1: startRow int
 	param 2: numRows int
+	return:java.util.List<net.gdface.facelog.db.LogLightBean>
 38 name: loadPersonByWhere 
 	param 0: where class java.lang.String
+	return:java.util.List<java.lang.Integer>
 39 name: loadPersonIdByUpdate 
 	param 0: timestamp long
+	return:java.util.List<java.lang.Integer>
 40 name: loadUpdatePersons 
 	param 0: timestamp long
+	return:java.util.List<java.lang.Integer>
 41 name: replaceFeature 
 	param 0: personId class java.lang.Integer
 	param 1: featureMd5 class java.lang.String
 	param 2: deleteOldFeatureImage boolean
+	return:void
 42 name: saveDevice 
 	param 0: deviceBean class net.gdface.facelog.db.DeviceBean
+	return:class net.gdface.facelog.db.DeviceBean
 43 name: savePerson 
 	param 0: bean class net.gdface.facelog.db.PersonBean
+	return:class net.gdface.facelog.db.PersonBean
 44 name: savePersonFull original name: savePerson 
 	param 0: bean class net.gdface.facelog.db.PersonBean
 	param 1: idPhoto class java.nio.ByteBuffer
@@ -122,44 +166,67 @@ IFaceLog
 	param 3: featureImage class java.nio.ByteBuffer
 	param 4: featureFaceBean class net.gdface.facelog.db.FaceBean
 	param 5: deviceId class java.lang.Integer
+	return:class net.gdface.facelog.db.PersonBean
 45 name: savePersonList original name: savePerson 
 	param 0: beans java.util.List<net.gdface.facelog.db.PersonBean>
+	return:void
 46 name: savePersonWithPhoto original name: savePerson 
 	param 0: bean class net.gdface.facelog.db.PersonBean
 	param 1: idPhoto class java.nio.ByteBuffer
+	return:class net.gdface.facelog.db.PersonBean
 47 name: savePersonWithPhotoAndFeature original name: savePerson 
 	param 0: bean class net.gdface.facelog.db.PersonBean
 	param 1: idPhoto class java.nio.ByteBuffer
 	param 2: featureBean class net.gdface.facelog.db.FeatureBean
 	param 3: deviceId class java.lang.Integer
+	return:class net.gdface.facelog.db.PersonBean
 48 name: savePersonWithPhotoAndFeatureMultiFaces original name: savePerson 
 	param 0: bean class net.gdface.facelog.db.PersonBean
 	param 1: idPhoto class java.nio.ByteBuffer
 	param 2: feature class java.nio.ByteBuffer
 	param 3: faceBeans java.util.List<net.gdface.facelog.db.FaceBean>
+	return:class net.gdface.facelog.db.PersonBean
 49 name: savePersonWithPhotoAndFeatureMultiImage original name: savePerson 
 	param 0: bean class net.gdface.facelog.db.PersonBean
 	param 1: idPhoto class java.nio.ByteBuffer
 	param 2: feature class java.nio.ByteBuffer
 	param 3: faceInfo java.util.Map<java.nio.ByteBuffer, net.gdface.facelog.db.FaceBean>
 	param 4: deviceId class java.lang.Integer
+	return:class net.gdface.facelog.db.PersonBean
 50 name: savePersonWithPhotoAndFeatureSaved original name: savePerson 
 	param 0: bean class net.gdface.facelog.db.PersonBean
 	param 1: idPhotoMd5 class java.lang.String
 	param 2: featureMd5 class java.lang.String
+	return:class net.gdface.facelog.db.PersonBean
 51 name: savePersonsWithPhoto original name: savePerson 
 	param 0: persons java.util.Map<java.nio.ByteBuffer, net.gdface.facelog.db.PersonBean>
+	return:class java.lang.Integer
 52 name: setPersonExpiryDate 
 	param 0: personId int
 	param 1: expiryDate long
+	return:void
 53 name: setPersonExpiryDateList original name: setPersonExpiryDate 
 	param 0: personIdList java.util.List<java.lang.Integer>
 	param 1: expiryDate long
+	return:void
 
 */
 
 package net.gdface.facelog.client;
+import com.facebook.nifty.client.FramedClientConnector;
+import com.facebook.swift.service.ThriftClientManager;
+import static com.google.common.net.HostAndPort.fromParts;
 
-public class Service implements Constant{
-
+public class IFaceLogClient implements Constant{
+    private final ThriftClientManager clientManager = new ThriftClientManager();
+    private final net.gdface.facelog.client.thrift.IFaceLog service;
+    public IFaceLogClient(String host,int port){
+        try{
+            service = clientManager.createClient(
+                    new FramedClientConnector(fromParts(host, port)),
+                    net.gdface.facelog.client.thrift.IFaceLog.class).get();
+		    } catch (Exception e) {
+		    	throw new RuntimeException(e);
+		    }
+    }
 }
