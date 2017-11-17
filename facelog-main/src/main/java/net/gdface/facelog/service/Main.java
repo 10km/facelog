e887c6419374f763e82c14818bc10aba3754140f
10km <10km0811@sohu.com>
2017-11-17 17:28:04 +0800

    commit

--- facelog-main/src/main/java/net/gdface/facelog/service/Main.java
+++ facelog-main/src/main/java/net/gdface/facelog/service/Main.java
@@ -3,6 +3,9 @@ package net.gdface.facelog.service;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.jboss.netty.logging.InternalLoggerFactory;
+import org.jboss.netty.logging.Slf4JLoggerFactory;
+
 /**
  * @author guyadong
  *
@@ -17,9 +20,10 @@ public class Main {
 		Server server = Server.bulider()
 				.withServices(new FaceLogImpl())
 				.setExecutor(pool).build();
+		// 设置slf4j记录日志,否则会有警告
+		InternalLoggerFactory.setDefaultFactory(new Slf4JLoggerFactory());
 		server.getServer().start();
 		}finally{
-//			pool.shutdown();
 		}
 	}	
 }
