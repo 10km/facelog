39efbe2fa18b9ecc31bac1dadbfd9e4ed8d09289
10km <10km0811@sohu.com>
2017-11-18 17:25:17 +0800

    commit

---
+++ facelog-client/src/test/java/net/gdface/facelog/client/NetworkUtilTest.java
@@ -0,0 +1,25 @@
+package net.gdface.facelog.client;
+
+import static org.junit.Assert.*;
+
+import java.net.NetworkInterface;
+
+import org.junit.Test;
+import org.weakref.jmx.com.google.common.base.Joiner;
+
+import com.google.common.collect.Iterators;
+
+import net.gdface.facelog.device.NetworkUtil;
+
+public class NetworkUtilTest {
+
+	@Test
+	public void test() {
+		for(NetworkInterface nic:NetworkUtil.getNICs(NetworkUtil.Filter.PHYICAL_ONLY,NetworkUtil.Filter.UP)){
+			System.out.printf("%s,MAC: %s\n",nic,NetworkUtil.getMacAddress(nic,"-"));
+			System.out.printf("\t%s\n", Joiner.on(",\n\t").join(Iterators.forEnumeration(nic.getInetAddresses())));
+			System.out.printf("\t%s\n", Joiner.on(",\n\t").join((nic.getInterfaceAddresses())));
+		}
+	}
+
+}
