4b964f1aed3695b3fe6ccdf03f42844a1ac2d4e2
10km <10km0811@sohu.com>
2017-12-02 00:15:13 +0800

    commit

--- facelog-client/src/main/java/net/gdface/facelog/client/IAckAdapter.java
+++ facelog-client/src/main/java/net/gdface/facelog/client/IAckAdapter.java
@@ -39,7 +39,7 @@ public interface IAckAdapter <T> extends IMessageAdapter<Ack<T>>{
      * @param <T>
      */
     public static class BaseAdapter<T> implements IAckAdapter<T> {
-    	/** 收到命令的client端数量 */
+    	/** 收到命令的client端数量,初始值-1,只有{@link #setClientNum(long)}被调用后才有效 */
         protected final AtomicLong clientNum = new AtomicLong(-1L);
         /**  
          * 命令响应等待有效期时间戳(毫秒), 参见 {@link System#currentTimeMillis()},
@@ -90,21 +90,29 @@ public interface IAckAdapter <T> extends IMessageAdapter<Ack<T>>{
 		 */
 		@Override
         public void onSubscribe(Ack<T> t) throws SmqUnsubscribeException {
-			if(t.getStatus() == Status.TIMEOUT){
-				// 超时处理
+			if(t.getStatus() == Status.TIMEOUT ){
 				// 发送这个状态的本机线程会自动取消频道订阅
+				if(ackCount !=clientNum.get()){
+					// 超时处理
+				}else{
+					// 超时之前已经收到了所有响应，但因为没有及时调用setClientNum造成的延迟
+					// 在设备端响应命令很快时有可能会出现这种情况
+					// 或者收到命令响应的设备端为0,也会引起超时
+				}
 			}else if(++ackCount ==clientNum.get()){
             	// 所有收到命令的设备都已经响应则抛出SmqUnsubscribeException异常用于取消当前频道订阅
                 throw new SmqUnsubscribeException(true);
             }
+			// 响应处理业务逻辑 。。。
         }
 
         /**
          * 只能被调用一次,否则第二次会抛出异常,
-         * 设备命令发送后，REDIS会返回收到设备命令的设备端数量
+         * 设备命令发送后，REDIS会返回收到设备命令的设备端数量,{@code clientNum}必须>=0
          */
         @Override
         public BaseAdapter<T> setClientNum(long clientNum){
+        	checkArgument(clientNum >= 0,"INVALID clientNum %s",clientNum);
         	checkState(this.clientNum.compareAndSet(-1L, clientNum),"clientNum can be set once only");    
             return this;
         }
