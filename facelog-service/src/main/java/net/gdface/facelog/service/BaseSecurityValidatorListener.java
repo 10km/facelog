cb14031d0f72299b4efc365d73ccb06400dd5b38
10km <10km0811@sohu.com>
2017-12-07 23:22:36 +0800

    commit

--- facelog-service/src/main/java/net/gdface/facelog/service/BaseSecurityValidatorListener.java
+++ facelog-service/src/main/java/net/gdface/facelog/service/BaseSecurityValidatorListener.java
@@ -1,15 +1,17 @@
 package net.gdface.facelog.service;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Set;
 
-import org.weakref.jmx.com.google.common.collect.Sets;
-
+import com.google.common.collect.Sets;
 import com.google.common.collect.Iterators;
 import com.google.common.collect.Lists;
 
 import com.google.common.base.Function;
-import com.google.common.base.Preconditions;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
 import com.google.common.base.Predicates;
 
 import net.gdface.facelog.db.BaseBean;
@@ -18,31 +20,50 @@ import net.gdface.facelog.db.TableListener;
 import net.gdface.facelog.db.exception.RuntimeDaoException;
 import net.gdface.facelog.service.Dao.PersonRank;
 import net.gdface.facelog.service.Dao.WriteOp;
+import net.gdface.facelog.service.Token.TokenType;
 
 /**
+ * 基于侦听器{@link TableListener}实现数据库记录在增加/删除/更新之前对操作令牌进行权限检查<br>
+ * 权限检查未通过则将{@link ServiceSecurityException}封装在{@link RuntimeDaoException}抛出
  * @author guyadong
  *
  */
 abstract class BaseSecurityValidatorListener<B extends BaseBean<B>> extends TableListener.Adapter<B> implements ServiceConstant {
 	protected final Dao dao;
 	protected final TlsTokenHandler tlsToken = TlsTokenHandler.INSTANCE;
-	BaseSecurityValidatorListener(Dao dao) {
-		this.dao = Preconditions.checkNotNull(dao,"dao is null");
+	private final Set<WriteOp> operatorAllow;
+	private final Set<WriteOp> deviceAllow;
+	private final Class<B> type;
+	@SuppressWarnings("unchecked")
+	protected BaseSecurityValidatorListener(Dao dao) {
+		this.dao = checkNotNull(dao,"dao is null");
+		operatorAllow = getAllowFromConfig(getOperatorAllowKey());
+		deviceAllow = getAllowFromConfig(getDeviceAllowKey());
+        Type superClass = getClass().getGenericSuperclass();
+        this.type = (Class<B>) ((ParameterizedType) superClass).getActualTypeArguments()[0];
 	}
-	protected abstract String getAllKey();
 	/**
-	 * 返回{@link Token}代表的用户等级
-	 * @param token
+	 * 返回读取配置文件中允许操作员操作的KEY
 	 * @return
 	 */
-	protected PersonRank rankFrom(Token token){
-		switch(token.getType()){
+	protected abstract String getOperatorAllowKey();
+	/**
+	 * 返回读取配置文件中允许设备操作的KEY
+	 * @return
+	 */
+	protected abstract String getDeviceAllowKey();
+	/**
+	 * 返回{@link Token}代表的用户等级
+	 * @return
+	 */
+	protected PersonRank rankFromToken(){
+		switch(tlsToken.get().getType()){
 		case ROOT:
 			return PersonRank.root;
 		case DEVICE:
 			return null;
 		case PERSON:{
-			PersonBean bean = dao.daoGetPerson(token.getId());
+			PersonBean bean = dao.daoGetPerson(tlsToken.get().getId());
 			if(null != bean){
 				return PersonRank.fromRank(bean.getRank());
 			}
@@ -51,7 +72,7 @@ abstract class BaseSecurityValidatorListener<B extends BaseBean<B>> extends Tabl
 		}
 		return null;
 	}
-	protected Set<WriteOp> getAllowFromConfig(String key){
+	protected static final Set<WriteOp> getAllowFromConfig(String key){
 		List<String> list = GlobalConfig.getExplodedStringAsList(CONFIG.getString(key,""));
 		List<WriteOp> opList = Lists.transform(list, new Function<String,Dao.WriteOp>(){
 			@Override
@@ -64,52 +85,78 @@ abstract class BaseSecurityValidatorListener<B extends BaseBean<B>> extends Tabl
 			}});
 		return Sets.newHashSet(Iterators.filter(opList.iterator(), Predicates.notNull()));
 	}
-	private void checkWriteOpForOperatorByConfig(PersonRank opRank,WriteOp writeOp){
-		if(opRank== PersonRank.operator 
-				&& !getAllowFromConfig(getAllKey()).contains(writeOp)){
+	/**
+	 * 根据配置文件中的设置检查是否允许{@code opRank}级别的用户操作执行{@code writeOp}指定的操作
+	 * @param writeOp
+	 */
+	protected final void checkWriteOpForPersonToken(WriteOp writeOp){
+		boolean deny = false;
+		PersonRank opRank =  rankFromToken();
+		if(null == opRank || opRank.rank < PersonRank.operator.rank ){
+			// 非管理员/操作员无权操作
+			deny = true;
+		}else if(opRank== PersonRank.operator 
+				&& !operatorAllow.contains(writeOp)){
+			deny = true;
+		}
+		if(deny){
 			throw new RuntimeDaoException(
-					new ServiceSecurityException(String.format("operator no  %s permission ,defined in config",writeOp)));
+					new ServiceSecurityException(
+							String.format("%s no  %s permission for %s",
+									opRank,writeOp,type.getSimpleName())));
 		}
 	}
-	private void checkWriteOpForDeviceByConfig(WriteOp writeOp, int deviceId){
-		if(!getAllowFromConfig(getAllKey()).contains(writeOp)){
+	/**
+	 * 根据配置文件中的设置检查是否允许设备执行{@code writeOp}指定的操作
+	 * @param writeOp
+	 */
+	protected final void checkWriteOpForDeviceToken(WriteOp writeOp){
+		if(!deviceAllow.contains(writeOp)){
 			throw new RuntimeDaoException(
-					new ServiceSecurityException(String.format("device no permission to create person,defined in config",writeOp))
-						.setDeviceID(deviceId));
+					new ServiceSecurityException(
+						String.format("device no %s permission for %s"
+								,writeOp,type.getSimpleName()))
+						.setDeviceID(tlsToken.get().getId()));
 		}
 	}
-	protected void check(B bean, WriteOp writeOp){
-		Token token = tlsToken.get();
-		switch(token.getType()){
+	/**
+	 * 检查当前令牌是否有写指定的写操作权限
+	 * @param bean
+	 * @param writeOp
+	 */
+	protected final void checkWriteOp(B bean, WriteOp writeOp){
+		TokenType tokenType = tlsToken.get().getType();
+		checkState(null !=tokenType,"typeToken is null");
+		switch(tokenType){
 		case DEVICE:{
-			checkWriteOpForDeviceByConfig(writeOp, token.getId());
+			checkWriteOpForDeviceToken(writeOp);
 			break;
 		}
 		case PERSON:{
-			PersonRank opRank =  rankFrom(token);
-			checkWriteOpForOperatorByConfig(opRank, writeOp);
+			checkWriteOpForPersonToken(writeOp);
 			break;
 		}
 		case ROOT:
 			break;
 		case UNINITIALIZED:
 		default:
-			throw new RuntimeDaoException(new ServiceSecurityException("INVALID token"));
+			throw new RuntimeDaoException(
+					new ServiceSecurityException(String.format("INVALID token %s",tokenType)));
 		}
 	}
 	@Override
 	public void beforeInsert(B bean) throws RuntimeDaoException {
-		check(bean, WriteOp.insert);
+		checkWriteOp(bean, WriteOp.insert);
 	}
 
 	@Override
 	public void beforeUpdate(B bean) throws RuntimeDaoException {
-		check(bean, WriteOp.update);
+		checkWriteOp(bean, WriteOp.update);
 	}
 
 	@Override
 	public void beforeDelete(B bean) throws RuntimeDaoException {
-		check(bean, WriteOp.delete);
+		checkWriteOp(bean, WriteOp.delete);
 	}
 
 }
--- facelog-service/src/main/java/net/gdface/facelog/service/SecurityValidatorPersonListener.java
+++ facelog-service/src/main/java/net/gdface/facelog/service/SecurityValidatorPersonListener.java
@@ -1,168 +1,66 @@
 package net.gdface.facelog.service;
 
-import java.util.List;
-import java.util.Set;
-
-import org.weakref.jmx.com.google.common.collect.Sets;
-
-import com.google.common.collect.Iterators;
-import com.google.common.collect.Lists;
-
-import com.google.common.base.Function;
-import com.google.common.base.Preconditions;
-import com.google.common.base.Predicates;
-
 import net.gdface.facelog.db.PersonBean;
-import net.gdface.facelog.db.TableListener;
 import net.gdface.facelog.db.exception.RuntimeDaoException;
 import net.gdface.facelog.service.Dao.PersonRank;
-import net.gdface.facelog.service.Dao.WriteOp;
+import net.gdface.facelog.service.Token.TokenType;
 
 /**
+ * fl_person表权限验证侦听器实现
  * @author guyadong
  *
  */
-class SecurityValidatorPersonListener extends TableListener.Adapter<PersonBean> implements ServiceConstant {
-	private final Dao dao;
-	private final TlsTokenHandler tlsToken = TlsTokenHandler.INSTANCE;
+class SecurityValidatorPersonListener extends BaseSecurityValidatorListener<PersonBean> {
 	SecurityValidatorPersonListener(Dao dao) {
-		this.dao = Preconditions.checkNotNull(dao,"dao is null");
+		super(dao);
 	}
-	/**
-	 * 返回{@link Token}代表的用户等级
-	 * @param token
-	 * @return
-	 */
-	private PersonRank rankFrom(Token token){
-		switch(token.getType()){
-		case ROOT:
-			return PersonRank.root;
-		case DEVICE:
-			return null;
-		case PERSON:{
-			PersonBean bean = dao.daoGetPerson(token.getId());
-			if(null != bean){
-				return PersonRank.fromRank(bean.getRank());
-			}
-		}			
-		default:
-		}
-		return null;
-	}
-	private Set<WriteOp> getAllowFromConfig(String key){
-		List<String> list = GlobalConfig.getExplodedStringAsList(CONFIG.getString(key,""));
-		List<WriteOp> opList = Lists.transform(list, new Function<String,Dao.WriteOp>(){
-			@Override
-			public WriteOp apply(String input) {
-				try{
-					return WriteOp.valueOf(input.toLowerCase());
-				}catch(RuntimeException e){
-					return null;
-				}
-			}});
-		return Sets.newHashSet(Iterators.filter(opList.iterator(), Predicates.notNull()));
-	}
-	private void checkWriteOpForOperatorByConfig(PersonRank opRank,String key, WriteOp writeOp){
-		if(opRank== PersonRank.operator 
-				&& !getAllowFromConfig(key).contains(writeOp)){
-			// 无权创建用户
-			throw new RuntimeDaoException(new ServiceSecurityException("operator no permission to create person,defined in config"));
-		}
-	}
-	@Override
-	public void beforeInsert(PersonBean bean) throws RuntimeDaoException {
+	private void checkForPersonToken(PersonBean bean){
 		Token token = tlsToken.get();
-		switch(token.getType()){
-		case DEVICE:{
+		if(token.getType() == TokenType.PERSON){
+			PersonRank opRank =  rankFromToken();
+			PersonRank rank = PersonRank.fromRank(bean.getRank());
+			if( null != rank && opRank.rank <= rank.rank ){
+				// 不可修改同级或高级用户
+				throw new RuntimeDaoException(new ServiceSecurityException("NO PERMISSION to delete admin account"));
+			}
+		}
+	}
+	private void checkInsertForDeviceToken(PersonBean bean){
+		Token token = tlsToken.get();
+		if(token.getType() == TokenType.DEVICE){
 			PersonRank rank = PersonRank.fromRank(bean.getRank());
 			if(!PersonRank.person.equals(rank)){
 				// 不允许从设备端创建管理员/操作员用户
 				throw new RuntimeDaoException(
 						new ServiceSecurityException("CANT create operator or admin user from device").setDeviceID(token.getId()));
 			}
-			break;
 		}
-		case PERSON:{
-			PersonRank rank = PersonRank.fromRank(bean.getRank());
-			PersonRank opRank =  rankFrom(token);
-			if(null == opRank || opRank.rank < PersonRank.operator.rank ){
-				// 非管理员/操作员无权创建用户
-				throw new RuntimeDaoException(new ServiceSecurityException("NO PERMISSION to create person"));
-			}
-			if( null != rank && opRank.rank <= rank.rank ){
-				// 不可创建同级或高级用户
-				throw new RuntimeDaoException(new ServiceSecurityException("NO PERMISSION to create admin account"));
-			}
-			checkWriteOpForOperatorByConfig(opRank, SECURITY_OPERATOR_TABLE_PERSON_ALLOW, WriteOp.insert);
-			break;
-		}
-		case ROOT:
-			break;
-		case UNINITIALIZED:
-		default:
-			throw new RuntimeDaoException(new ServiceSecurityException("INVALID token"));
-		}
+	}
+	@Override
+	public void beforeInsert(PersonBean bean) throws RuntimeDaoException {
+		super.beforeUpdate(bean);
+		checkForPersonToken(bean);
+		checkInsertForDeviceToken(bean);
 	}
 
 	@Override
 	public void beforeUpdate(PersonBean bean) throws RuntimeDaoException {
-		Token token = tlsToken.get();
-		switch(token.getType()){
-		case DEVICE:{
-			// 不允许从设备端执行删除用户操作
-			throw new RuntimeDaoException(new ServiceSecurityException("CANT update user from device").setDeviceID(token.getId()));
-		}
-		case PERSON:{
-			PersonRank rank = PersonRank.fromRank(bean.getRank());
-			PersonRank opRank =  rankFrom(token);
-			if(null == opRank || opRank.rank < PersonRank.operator.rank ){
-				// 非管理员/操作员无权删除用户
-				throw new RuntimeDaoException(new ServiceSecurityException("NO PERMISSION to delete person"));
-			}
-			if( null != rank && opRank.rank <= rank.rank ){
-				// 不可删除同级或高级用户
-				throw new RuntimeDaoException(new ServiceSecurityException("NO PERMISSION to delete admin account"));
-			}
-			checkWriteOpForOperatorByConfig(opRank, SECURITY_OPERATOR_TABLE_PERSON_ALLOW, WriteOp.update);
-			break;
-		}
-		case ROOT:
-			break;
-		case UNINITIALIZED:
-		default:
-			throw new RuntimeDaoException(new ServiceSecurityException("INVALID token"));
-		}
+		super.beforeUpdate(bean);
+		checkForPersonToken(bean);
 	}
 
 	@Override
 	public void beforeDelete(PersonBean bean) throws RuntimeDaoException {
-		Token token = tlsToken.get();
-		switch(token.getType()){
-		case DEVICE:{
-			// 不允许从设备端执行删除用户操作
-			throw new RuntimeDaoException(new ServiceSecurityException("CANT delete user from device").setDeviceID(token.getId()));
-		}
-		case PERSON:{
-			PersonRank rank = PersonRank.fromRank(bean.getRank());
-			PersonRank opRank =  rankFrom(token);
-			if(null == opRank || opRank.rank < PersonRank.operator.rank ){
-				// 非管理员/操作员无权删除用户
-				throw new RuntimeDaoException(new ServiceSecurityException("NO PERMISSION to delete person"));
-			}
-			if( null != rank && opRank.rank <= rank.rank ){
-				// 不可删除同级或高级用户
-				throw new RuntimeDaoException(new ServiceSecurityException("NO PERMISSION to delete admin account"));
-			}
-			checkWriteOpForOperatorByConfig(opRank, SECURITY_OPERATOR_TABLE_PERSON_ALLOW, WriteOp.delete);
-			break;
-		}
-		case ROOT:
-			break;
-		case UNINITIALIZED:
-		default:
-			throw new RuntimeDaoException(new ServiceSecurityException("INVALID token"));
-		}
-	
+		super.beforeDelete(bean);
+		checkForPersonToken(bean);
+	}
+	@Override
+	protected String getOperatorAllowKey() {
+		return SECURITY_OPERATOR_TABLE_PERSON_ALLOW;
+	}
+	@Override
+	protected String getDeviceAllowKey() {
+		return SECURITY_DEVICE_TABLE_PERSON_ALLOW;
 	}
 
 }
