3615456e5db67d7b0765650b6ee15d6381dac9f2
10km <10km0811@sohu.com>
2017-12-18 11:26:43 +0800

    更新 idl-generator-cli 版本

--- facelog-service/gen_ifacelog_thrift.bat
+++ facelog-service/gen_ifacelog_thrift.bat
@@ -1,4 +1,4 @@
-java -cp lib\idl-generator-cli-1.1-standalone.jar;target\classes;..\db\target\classes;..\simplemq\target\classes ^
+java -cp lib\idl-generator-cli-1.2-standalone.jar;target\classes;..\db\target\classes;..\simplemq\target\classes ^
     com.facebook.swift.generator.swift2thrift.custom.Main ^
     -namespace py gdface.thrift ^
     -namespace java com.gdface -namespace cpp gdface ^
