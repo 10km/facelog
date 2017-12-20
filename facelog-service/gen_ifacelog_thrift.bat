b0e11130ffd383608429a224dc07900664e4b889
10km <10km0811@sohu.com>
2017-12-20 14:52:38 +0800

    commit

--- facelog-service/gen_ifacelog_thrift.bat
+++ facelog-service/gen_ifacelog_thrift.bat
@@ -1,7 +1,8 @@
 java -cp lib\idl-generator-cli-1.5-standalone.jar;target\classes;..\db\target\classes;..\simplemq\target\classes ^
     com.gitee.l0km.thrift.swift2thrift.generator.Main ^
+    -namespace cpp gdface ^
+    -namespace java com.gdface ^
     -namespace py gdface.thrift ^
-    -namespace java com.gdface -namespace cpp gdface ^
     -out IFaceLog.thrift ^
     -package net.gdface.facelog.service BaseFaceLog ^
     -recursive
