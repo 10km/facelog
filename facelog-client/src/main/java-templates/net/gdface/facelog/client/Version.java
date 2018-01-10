3d9aa44ffa591b327df22ab69403e6650456feac
10km <10km0811@sohu.com>
2018-01-10 10:39:32 +0800

    Version.java 模板中增加 SCM_BRANCH 字段

--- facelog-client/src/main/java-templates/net/gdface/facelog/client/Version.java
+++ facelog-client/src/main/java-templates/net/gdface/facelog/client/Version.java
@@ -11,11 +11,14 @@ public final class Version {
     public static final String VERSION = "${project.version}";
     /** SCM(git) revision */
     public static final String SCM_REVISION= "${buildNumber}";
+    /** SCM branch */
+    public static final String SCM_BRANCH = "${scmBranch}";
     /** build timestamp */
     public static final String TIMESTAMP ="${buildtimestamp}";
     /** map of version fields */
     public static final ImmutableMap<String,String> INFO = ImmutableMap.of(
     		"VERSION", VERSION, 
     		"SCM_REVISION", SCM_REVISION, 
+    		"SCM_BRANCH",SCM_BRANCH,
     		"TIMESTAMP", TIMESTAMP);
 }
