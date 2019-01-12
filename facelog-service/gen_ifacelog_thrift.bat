bfbdcab4f2995d6dc8082e9650b89eea27892aef
10km <10km0811@sohu.com>
2019-01-12 14:46:18 +0800

    update for swift2thrift-maven-plugin

--- facelog-service/gen_ifacelog_thrift.bat
+++ facelog-service/gen_ifacelog_thrift.bat
@@ -1,4 +1,4 @@
-  java -cp lib\idl-generator-cli-1.8-SNAPSHOT-standalone.jar;target\classes;..\db\target\classes;..\simplemq\target\classes ^
+  java -cp lib\idl-generator-cli-1.8-standalone.jar;target\classes;..\db\target\classes;..\simplemq\target\classes ^
      com.gitee.l0km.thrift.swift2thrift.generator.Main ^
      -namespace cpp gdface ^
      -namespace java net.gdface.facelog.client.thrift ^
@@ -7,4 +7,4 @@
      -package net.gdface.facelog.service BaseFaceLog ^
      -recursive ^
      -objectOptional
-rem mvn com.gitee.l0km:swift2thrift-maven-plugin:1.8-SNAPSHOT:generate
+rem mvn com.gitee.l0km:swift2thrift-maven-plugin:1.8:generate
