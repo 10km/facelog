3eee18bbebb856fc40b78d7425abe41f0d2d29dd
10km <10km0811@sohu.com>
2019-01-16 18:22:25 +0800

    commit

---
+++ facelog-local/src/main/java/net/gdface/facelog/ServiceConstant.java
@@ -0,0 +1,55 @@
+package net.gdface.facelog;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledExecutorService;
+
+import org.apache.commons.configuration2.CombinedConfiguration;
+
+import gu.simplemq.Channel;
+import net.gdface.facelog.service.CommonConstant;
+
+/**
+ * 服务端常量定义
+ * @author guyadong
+ *
+ */
+public interface ServiceConstant extends CommonConstant{
+	/** 设备访问令牌表,{@code 设备ID -> token}  */
+    public static final Channel<Token> TABLE_DEVICE_TOKEN = new Channel<Token>("DeviceToken"){} ;
+    /** 人员访问令牌表 {@code 人员ID -> token} */
+    public static final Channel<Token> TABLE_PERSON_TOKEN = new Channel<Token>("PersonToken"){} ;
+    /** 设备命令序列号 {@code cmd sn -> 人员ID} */
+    public static final Channel<Integer> TABLE_CMD_SN = new Channel<Integer>("CmdSN"){} ;
+    /** 设备响应通道 {@code channel -> 人员ID} */
+    public static final Channel<Integer> TABLE_ACK_CHANNEL = new Channel<Integer>("AckChannel"){} ;
+
+    /////////////  REDIS KEYS ////////////////////////////
+	/** redis 全局变量 : 设备命令序列号, 调用{@link TokenMangement#applyCmdSn(Token)}时每次加1,保证序列号在全网络唯一 */
+	String KEY_CMD_SN = "CMD_SN";
+	/** redis 全局变量 : 设备响应通道序列号, 调用{@link TokenMangement#applyAckChannel(Token, long)}时每次加1,保证序列号在全网络唯一 */
+	String KEY_ACK_SN = "ACK_CHANNEL";
+	/** redis 全局常量 : 设备命令通道 */
+	String KEY_CMD_CHANNEL = "CMD_CHANNEL";
+	/** redis 全局常量 : 人员验证实时监控通道 */
+	String KEY_LOG_MONITOR_CHANNEL = "LOG_MONITOR_CHANNEL";
+	/** redis 全局常量 : 设备心跳实时监控通道 */
+	String KEY_HB_MONITOR_CHANNEL = "HB_MONITOR_CHANNEL";
+    ///////////// PROPERTY KEY DEFINITION (不对 client 公开的 key 定义)///////////
+	
+    /** salt for password */
+    public static final String TOKEN_SALT = "token.salt";
+
+	/////////////////////////////////////////////////
+	
+	/** database 配置属性前缀 */
+	public static final String PREFIX_DATABASE = "database.";
+	
+	/** 全局配置参数对象 */
+	public static final CombinedConfiguration CONFIG = GlobalConfig.getConfig();
+	
+	/** 全局线程池(自动退出封装) */
+	public static final ExecutorService GLOBAL_EXCEUTOR = ExecutorProvider.getGlobalExceutor();
+
+	/** 定时任务线程池对象(自动退出封装) */
+	public static final ScheduledExecutorService TIMER_EXECUTOR = ExecutorProvider.getTimerExecutor();
+}
---
+++ facelog-local/src/main/java/net/gdface/facelog/TableManagerInitializer.java
@@ -0,0 +1,85 @@
+package net.gdface.facelog;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import net.gdface.facelog.db.Constant.JdbcProperty;
+import net.gdface.facelog.db.IDeviceGroupManager;
+import net.gdface.facelog.db.IDeviceManager;
+import net.gdface.facelog.db.IFaceManager;
+import net.gdface.facelog.db.IFeatureManager;
+import net.gdface.facelog.db.IImageManager;
+import net.gdface.facelog.db.ILogLightManager;
+import net.gdface.facelog.db.ILogManager;
+import net.gdface.facelog.db.IPermitManager;
+import net.gdface.facelog.db.IPersonGroupManager;
+import net.gdface.facelog.db.IPersonManager;
+import net.gdface.facelog.db.IStoreManager;
+import net.gdface.facelog.db.ITableCache.UpdateStrategy;
+import net.gdface.facelog.db.LogBean;
+import net.gdface.facelog.db.LogLightBean;
+import net.gdface.facelog.db.mysql.DeviceCacheManager;
+import net.gdface.facelog.db.mysql.DeviceGroupCacheManager;
+import net.gdface.facelog.db.mysql.FaceCacheManager;
+import net.gdface.facelog.db.mysql.FeatureCacheManager;
+import net.gdface.facelog.db.mysql.ImageCacheManager;
+import net.gdface.facelog.db.mysql.ManagerUtil;
+import net.gdface.facelog.db.mysql.PermitCacheManager;
+import net.gdface.facelog.db.mysql.PersonCacheManager;
+import net.gdface.facelog.db.mysql.PersonGroupCacheManager;
+import net.gdface.facelog.db.mysql.StoreCacheManager;
+import net.gdface.facelog.db.mysql.TableInstance;
+
+/**
+ * 全局初始化数据库访问对象(TableManager)
+ * @author guyadong
+ *
+ */
+public class TableManagerInitializer {
+	
+	public final IDeviceManager deviceManager;
+	public final IDeviceGroupManager deviceGroupManager;
+	public final IFaceManager faceManager ;
+	public final IImageManager imageManager ;
+	public final ILogManager logManager ;
+	public final ILogLightManager logLightManager ;
+	public final IPersonManager personManager ;
+	public final IPersonGroupManager personGroupManager;
+	public final IStoreManager storeManager ;
+	public final IFeatureManager featureManager ;
+	public final IPermitManager permitManager;
+	static{
+		// 向底层数据库操作类注入当前项目使用的数据库连接配置
+		Map<JdbcProperty, String> databaseConfig = GlobalConfig.makeDatabaseConfig();
+		//GlobalConfig.logDatabaseProperties(databaseConfig);
+		ManagerUtil.injectProperties(GlobalConfig.toStringKey(databaseConfig));
+	}
+	public static final TableManagerInitializer INSTANCE = new TableManagerInitializer();
+	private TableManagerInitializer() {
+		// 配置cache参数
+		// log,log_light 表因为不会被修改，也不会被设备频繁读取，不需要使用cache对象
+		// 有 CURRENT_TIMESTAMP属性的时间戳字段的表要更新策略为refresh,否则为always
+		deviceManager = DeviceCacheManager.makeInstance(UpdateStrategy.refresh,10000,60,TimeUnit.MINUTES);
+		deviceGroupManager = DeviceGroupCacheManager.makeInstance(UpdateStrategy.refresh,10000,60,TimeUnit.MINUTES);
+		faceManager = FaceCacheManager.makeInstance(UpdateStrategy.always,10000,10,TimeUnit.MINUTES);
+		imageManager = ImageCacheManager.makeInstance(UpdateStrategy.always,1000,10,TimeUnit.MINUTES);		 
+		logManager = (ILogManager) TableInstance.getInstance(LogBean.class);
+		logLightManager = (ILogLightManager) TableInstance.getInstance(LogLightBean.class);
+		personManager = PersonCacheManager.makeInstance(UpdateStrategy.refresh,10000,10,TimeUnit.MINUTES);
+		personGroupManager = PersonGroupCacheManager.makeInstance(UpdateStrategy.refresh,10000,60,TimeUnit.MINUTES);
+		storeManager = StoreCacheManager.makeInstance(UpdateStrategy.always,1000,10,TimeUnit.MINUTES);
+		featureManager = FeatureCacheManager.makeInstance(UpdateStrategy.refresh,10000,10,TimeUnit.MINUTES);
+		permitManager = PermitCacheManager.makeInstance(UpdateStrategy.refresh,10000,10,TimeUnit.MINUTES);
+		
+/*		deviceManager = (IDeviceManager) TableInstance.getInstance(DeviceBean.class);
+		faceManager = (IFaceManager) TableInstance.getInstance(FaceBean.class);
+		imageManager = (IImageManager) TableInstance.getInstance(ImageBean.class);
+		logManager = (ILogManager) TableInstance.getInstance(LogBean.class);
+		logLightManager = (ILogLightManager) TableInstance.getInstance(LogLightBean.class);
+		personManager = (IPersonManager) TableInstance.getInstance(PersonBean.class);
+		storeManager = (IStoreManager) TableInstance.getInstance(StoreBean.class);
+		featureManager = (IFeatureManager) TableInstance.getInstance(FeatureBean.class);*/
+	}	
+	
+
+}
