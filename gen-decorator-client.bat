f8f7bfcd97517db6564724755b7f8791ae8f70ea
10km <10km0811@sohu.com>
2019-01-16 15:56:40 +0800

    commit

--- gen-decorator-client.bat
+++ gen-decorator-client.bat
@@ -1,8 +1,11 @@
-java -cp lib\codegen-thrift-1.0.3-SNAPSHOT-standalone.jar;facelog-base\target\classes;db\facelog-db-base\target\classes;..\faceapi\faceapi-base\target\classes; ^
-    net.gdface.codegen.thrift.ThriftServiceDecoratorGenerator ^
-		-ic net.gdface.facelog.IFaceLog ^
-		-rc net.gdface.facelog.FacelogDefaultImpl ^
-		-p net.gdface.facelog.thrift ^
-		-o facelog-thriftclient\src\client\java ^
-		--thrift-package net.gdface.facelog.thrift.client ^
-		-gt CLIENT
+rem java -cp lib\codegen-thrift-1.0.3-SNAPSHOT-standalone.jar;facelog-base\target\classes;db\facelog-db-base\target\classes;..\faceapi\faceapi-base\target\classes; ^
+rem     net.gdface.codegen.thrift.ThriftServiceDecoratorGenerator ^
+rem 		-ic net.gdface.facelog.IFaceLog ^
+rem 		-rc net.gdface.facelog.FacelogDefaultImpl ^
+rem 		-p net.gdface.facelog.thrift ^
+rem 		-o facelog-thriftclient\src\client\java ^
+rem 		--thrift-package net.gdface.facelog.thrift.client ^
+rem 		-gt CLIENT		
+cd facelog-thriftclient
+mvn com.gitee.l0km:codegen-thrift-maven-plugin:1.0.3-SNAPSHOT:generate
+cd ..
