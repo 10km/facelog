42d462d32508cfa3426d3aa2b476681d4d31e28d
10km <10km0811@sohu.com>
2019-02-13 14:42:57 +0800

    use thrifty-compiler-maven-plugin instead of thrifty-compiler-1.0.0-all.jar

--- facelog-service/gen_client-thrifty.bat
+++ facelog-service/gen_client-thrifty.bat
@@ -1,7 +1,3 @@
-set OUT_FOLDER=..\facelog-client-android\src\thrifty\java
-rmdir  %OUT_FOLDER% /s/q
-REM Java 8 required
-java -jar lib\thrifty-compiler-1.0.0-all.jar ^
-	--out=%OUT_FOLDER% ^
-	IFaceLog.thrift 
-
+pushd ..\facelog-client-android
+mvn com.gitee.l0km:thrifty-compiler-maven-plugin:stub 	
+popd 
