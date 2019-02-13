26f690a2010121f4526797d6194194fbfc8a4916
10km <10km0811@sohu.com>
2019-02-13 14:56:52 +0800

    commit

--- facelog-service/gen_client-thrifty.bat
+++ facelog-service/gen_client-thrifty.bat
@@ -1,3 +1,7 @@
-pushd ..\facelog-client-android
+rem Java 8 required
+@set sh_folder=%~dp0
+@pushd %sh_folder%\..\facelog-client-android
+@set OUT_FOLDER=src\thrifty\java
+@if exist %OUT_FOLDER% rmdir  %OUT_FOLDER% /s/q
 mvn com.gitee.l0km:thrifty-compiler-maven-plugin:stub 	
-popd 
+@popd 
