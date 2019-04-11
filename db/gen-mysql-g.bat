7c48a7d3c27b75cf21a05ea26ee50a940509808e
10km <10km0811@sohu.com>
2019-04-11 22:54:31 +0800

    更新bat脚本，解决脚本执行结束时无法正确返回当前文件夹的问题

--- db/gen-mysql-g.bat
+++ db/gen-mysql-g.bat
@@ -1,4 +1,5 @@
 echo off
-cd sql2java
-mvn antrun:run -Dsql2java-config=../gen-mysql-g.properties -Dgenerated-src=../facelog-db-base/src/main/java -Ddriver-jar=../lib/mysql-connector-java-5.1.43-bin.jar
-cd ..
+set sh_folder=%~dp0
+pushd %sh_folder%sql2java
+call mvn antrun:run -Dsql2java-config=../gen-mysql-g.properties -Dgenerated-src=../facelog-db-base/src/main/java -Ddriver-jar=../lib/mysql-connector-java-5.1.43-bin.jar
+popd
--- db/gen-mysql.bat
+++ db/gen-mysql.bat
@@ -1,7 +1,8 @@
 echo off
-cd sql2java
+set sh_folder=%~dp0
+pushd %sh_folder%sql2java
 rem ant rebuild -Dsql2java-config=../gen-mysql.properties -Dgenerated-src=../src/orm/java
 rem ant rebuild -Dsql2java-config=../gen-mysql.properties -Dgenerated-src=../src/orm/java -Ddriver-lib=../lib 
 rem ant rebuild -Dsql2java-config=../gen-mysql.properties -Dgenerated-src=../src/orm/java -Ddriver-jar=../lib/mysql-connector-java-5.1.43-bin.jar
-mvn antrun:run -Dsql2java-config=../gen-mysql.properties -Dgenerated-src=../facelog-db-orm/src/main/java -Ddriver-jar=../lib/mysql-connector-java-5.1.43-bin.jar
-cd ..
+call mvn antrun:run -Dsql2java-config=../gen-mysql.properties -Dgenerated-src=../facelog-db-orm/src/main/java -Ddriver-jar=../lib/mysql-connector-java-5.1.43-bin.jar
+popd
