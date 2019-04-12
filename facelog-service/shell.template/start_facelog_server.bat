9730db2706dd6b36241f7255cdf666a559217c97
10km <10km0811@sohu.com>
2019-04-12 12:37:57 +0800

    将 start_facelog_server.bat, start_facelog_server_debug.bat 脚本改为模板生成，以自动匹配当前版本号

---
+++ facelog-service/shell.template/start_facelog_server.bat
@@ -0,0 +1,3 @@
+pushd %~dp0
+java -jar target\facelog-service-${project.version}-standalone.jar
+popd
--- facelog-service/start_facelog_server.bat
+++
@@ -1,3 +0,0 @@
-pushd %~dp0
-java -jar target\facelog-service-2.0.7-SNAPSHOT-standalone.jar
-popd
