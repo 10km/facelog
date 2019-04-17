28565460a12625a8cf165892470b3931939d2d47
10km <10km0811@sohu.com>
2019-04-17 15:12:31 +0800

    commit

--- facelog-client-ext/node.js/redis_test.js
+++ facelog-client-ext/node.js/redis_test.js
@@ -2,10 +2,16 @@
  * redis访问测试
  */
 var redis = require('redis');
-// var client = redis.createClient(6379,'localhost');
-var client = redis.createClient('redis://:hello@localhost:6379/0')
-client.set('hello','This is a value',(err,res)=>{
-    client.get('hello',(err,res)=>{
-        console.log('value:',res)
-    })
-});
+var client = redis.createClient(6379,'localhost');
+// 如果redis设置了密码，需要验证
+client.auth('hello')
+// 也可如下通过uri创建client实例
+//var client = redis.createClient('redis://:hello@localhost:6379/0')
+
+// 写入key-value
+client.set('hello','This is a value')
+
+// 读取value
+client.get('hello',(err,res)=>{
+    console.log('value:',res)
+})
