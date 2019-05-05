0ea3fc859f122ad9ca4a919d7911f33f1ff11f1d
10km <10km0811@sohu.com>
2019-05-05 10:35:36 +0800

    commit

--- facelog-service/src/main/java/net/gdface/service/facelog/FaceLogService.java
+++ facelog-service/src/main/java/net/gdface/service/facelog/FaceLogService.java
@@ -62,7 +62,7 @@ public class FaceLogService extends ThriftServerService implements CommonConstan
 		return httpService = buildService(httpService,
 				makeThriftServerConfig()
 					.setPort(GlobalConfig.getConfig().getInt(XHR_PORT, DEFAULT_PORT_XHR))
-					.setIdleConnectionTimeout(Duration.valueOf("10ms"))
+					.setIdleConnectionTimeout(Duration.valueOf("100ms"))
 					.setTransportName(ThriftServerService.HTTP_TRANSPORT)
 					.setProtocolName(ThriftServerService.JSON_PROTOCOL));
 	}
