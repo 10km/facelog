9fc20d57f25dd8bae10ddfe98510d554e165ab95
10km <10km0811@sohu.com>
2019-05-08 13:58:53 +0800

    COMMIT

--- facelog-client-android/src/test/java/net/gdface/facelog/client/HeartbeatTest.java
+++ facelog-client-android/src/test/java/net/gdface/facelog/client/HeartbeatTest.java
@@ -13,12 +13,10 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.ImmutableMap;
 
-import gu.simplemq.Channel;
 import gu.simplemq.IMessageAdapter;
 import gu.simplemq.exceptions.SmqUnsubscribeException;
 import gu.simplemq.redis.JedisPoolLazy;
 import gu.simplemq.redis.JedisPoolLazy.PropName;
-import gu.simplemq.redis.RedisFactory;
 import net.gdface.facelog.DeviceHeadbeatPackage;
 import net.gdface.facelog.MQParam;
 import net.gdface.facelog.Token;
@@ -52,11 +50,15 @@ public class HeartbeatTest implements ChannelConstant{
 		// 根据连接参数创建默认实例 
 		JedisPoolLazy.createDefaultInstance( redisParam);
 		// 创建服务实例
-		facelogClient = ClientFactory.builder().setHostAndPort("127.0.0.1", DEFAULT_PORT).build(IFaceLogThriftClient.class, IFaceLogClient.class);
+		facelogClient = ClientFactory.builder()
+				.setHostAndPort("127.0.0.1", DEFAULT_PORT)
+				.setDecorator(RefreshTokenDecorator.makeDecoratorFunction(TokenHelperTestImpl.INSTANCE))
+				.build(IFaceLogThriftClient.class, IFaceLogClient.class);
 		// 申请令牌
 		rootToken = facelogClient.applyRootToken("guyadong", false);
 		// 从facelog service 获取心跳监控频道名 
 		monitorChannelName = facelogClient.getRedisParameters(rootToken).get(MQParam.HB_MONITOR_CHANNEL);
+		facelogClient.initTokenSupplier(TokenHelperTestImpl.INSTANCE, rootToken);
 		logger.info("monitorChannelName = {}",monitorChannelName);
 	}
 	@AfterClass
@@ -70,12 +72,16 @@ public class HeartbeatTest implements ChannelConstant{
 	public void test1SendHB() {
 		Heartbeat hb = Heartbeat.makeHeartbeat(12345, JedisPoolLazy.getDefaultInstance())
 				/** 将设备心跳包数据发送到指定的设备心跳监控通道名,否则监控端无法收到设备心跳包 */
-				.setMonitorChannel(monitorChannelName);
+				.setMonitorChannelSupplier(facelogClient.getMonitorChannelSupplier(rootToken));
 		/** 以默认间隔启动定时任务 */
 		hb.start();
 		System.out.println("Heartbeat thead start");
 		/** 间隔2秒发送心跳，重新启动定时任务 */
-		hb.setInterval(2, TimeUnit.SECONDS).start();
+		try {
+			hb.setInterval(2, TimeUnit.SECONDS).start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	/**
 	 * 管理端心跳包监控测试
@@ -83,17 +89,31 @@ public class HeartbeatTest implements ChannelConstant{
 	 */
 	@Test
 	public void test2HBMonitor() throws InterruptedException{
-		Channel<DeviceHeadbeatPackage> hbMonitorChannel = new Channel<DeviceHeadbeatPackage>(monitorChannelName,
-				new IMessageAdapter<DeviceHeadbeatPackage>(){
+		IMessageAdapter<DeviceHeadbeatPackage> hbAdapter = new IMessageAdapter<DeviceHeadbeatPackage>(){
 			@Override
 			public void onSubscribe(DeviceHeadbeatPackage t) throws SmqUnsubscribeException {
 				// 显示收到的心跳包
 				logger.info(t.toString());
-			}}){};
-		// 注册，订阅设备心跳监控频道消息
-		RedisFactory.getSubscriber().register(hbMonitorChannel);
+			}};
 		
-		/** 20秒后结束测试 */
-		Thread.sleep(20*1000);
+		new HeartbeatMonitor(hbAdapter,facelogClient.getMonitorChannelSupplier(rootToken)).start();
+		/** 40秒后结束测试 */
+		Thread.sleep(60*1000);
+	}
+	public static class TokenHelperTestImpl extends TokenHelper {
+		final static TokenHelperTestImpl INSTANCE = new TokenHelperTestImpl(); 
+		public TokenHelperTestImpl() {
+		}
+
+		@Override
+		public String passwordOf(int id) {
+			return "guyadong";
+		}
+
+		@Override
+		public boolean isHashedPwd() {
+			return false;
+		}
+
 	}
 }
