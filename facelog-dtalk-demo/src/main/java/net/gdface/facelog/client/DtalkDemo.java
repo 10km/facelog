b035ed397950af0a378b00d4daf8c35a1311404f
10km <10km0811@sohu.com>
2019-05-30 19:22:10 +0800

    commit

--- facelog-dtalk-demo/src/main/java/net/gdface/facelog/client/DtalkDemo.java
+++ facelog-dtalk-demo/src/main/java/net/gdface/facelog/client/DtalkDemo.java
@@ -6,8 +6,6 @@ import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Strings;
-
 import gu.dtalk.exception.DtalkException;
 import gu.dtalk.redis.RedisConfigType;
 import net.gdface.facelog.ServiceSecurityException;
