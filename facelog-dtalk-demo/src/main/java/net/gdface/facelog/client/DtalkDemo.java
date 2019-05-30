2c0894ee31637e07858b5b3f79ca378538c7e8a5
10km <10km0811@sohu.com>
2019-05-30 19:21:42 +0800

    完成设备模拟器启动逻辑，允许脱网运行

--- facelog-dtalk-demo/src/main/java/net/gdface/facelog/client/DtalkDemo.java
+++ facelog-dtalk-demo/src/main/java/net/gdface/facelog/client/DtalkDemo.java
@@ -6,6 +6,9 @@ import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Strings;
+
+import gu.dtalk.exception.DtalkException;
 import gu.dtalk.redis.RedisConfigType;
 import net.gdface.facelog.ServiceSecurityException;
 import net.gdface.facelog.Token;
@@ -14,6 +17,7 @@ import net.gdface.facelog.client.dtalk.FacelogMenu;
 import net.gdface.facelog.client.location.ConnectConfigProvider;
 import net.gdface.facelog.client.location.ConnectConfigType;
 import net.gdface.facelog.client.location.DefaultCustomConnectConfigProvider;
+import net.gdface.facelog.client.location.FaceLogConnectException;
 import net.gdface.facelog.db.DeviceBean;
 import net.gdface.facelog.thrift.IFaceLogThriftClient;
 import net.gdface.thrift.ClientFactory;
@@ -39,18 +43,32 @@ public class DtalkDemo {
 	private DtalkEngineForFacelog engine;
 
 	private ConnectConfigProvider config;
-
+	
+	/**
+	 * 联网状态(接入facelog)构造方法<br>
+	 * @param facelogClient 
+	 * @param config
+	 */
 	public DtalkDemo(IFaceLogClient facelogClient, ConnectConfigProvider config) {
 		this.facelogClient = checkNotNull(facelogClient,"facelogClient is null");
-		this.config = config;
+		this.config = checkNotNull(config,"config is null");
 		this.redisConfigType = null;
 	}
+	/**
+	 * 脱网状态(非接入facelog)构造方法<br>
+	 * 
+	 * @param redisConfigType
+	 */
 	public DtalkDemo(RedisConfigType redisConfigType) {
 		this.facelogClient=null;
 		this.config = null;
 		this.redisConfigType = checkNotNull(redisConfigType,"redisConfigType is null");
 	}
-	private DtalkDemo initDevice() throws ServiceSecurityException{
+	/**
+	 * 初始化设备信息
+	 * @return 当前对象
+	 */
+	private DtalkDemo initDevice(){
 		devMac = DEVINFO_PROVIDER.getMac();
 
 		device = DeviceBean.builder()
@@ -59,6 +77,15 @@ public class DtalkDemo {
 				.usedSdks("MTFSDKARM512")
 				.build();
 		logger.info(device.toString(true,false));
+		return this;
+	}
+	/**
+	 * 接入facelog状态下设备上线
+	 * @return 当前对象
+	 * @throws ServiceSecurityException
+	 */
+	private DtalkDemo onlineDeviceIfFacelogPresent() throws ServiceSecurityException{
+		initDevice();
 		if(facelogClient !=null){
 			// 注册设备 
 			device = this.facelogClient.registerDevice(device);
@@ -76,8 +103,10 @@ public class DtalkDemo {
 	private void start() {
 		FacelogMenu root = FacelogMenu.makeActiveInstance(config).init().register(DemoListener.INSTANCE);
 		if(facelogClient != null){
+			logger.info("Dtalk Device connect into facelog(设备接入facelog)");
 			engine = facelogClient.initDtalkEngine(deviceToken, root);
 		}else{
+			logger.info("Dtalk Device running without facelog(设备离线运行)");
 			engine = new DtalkEngineForFacelog(root,redisConfigType);
 		}
 		engine.start();
@@ -126,6 +155,14 @@ public class DtalkDemo {
 		}
 		
 	}
+	/**
+	 * 模拟器启动流程:<br>
+	 * 1.解析命令行参数，如果提供了facelog主机名和端口则使用指定的参数连接facelog<br>
+	 * 2.如果命令行没有提供facelog主机名和端口号则启动facelog自动发现机制寻找facelog服务,如果找到facelog服务则以联网方式(接入facelog)运行<br>
+	 * 3.如果没有找到faceog服务，则启动dtalk自动发现机制自动寻找redis服务，如果找到redis则以脱网方式(非接入facelog)运行<br>
+	 * 4.如果没有找到redis，则报错退出，模拟器无法运行<br>
+	 * @param args
+	 */
 	public static void main(String []args){
 		CONSOLE_CONFIG.parseCommandLine(args);
 		System.out.println("Dtalk simulator for Facelog Device is starting(facelog设备dtalk模拟器启动)");
@@ -135,28 +172,46 @@ public class DtalkDemo {
 		ConnectConfigType type;
 		try{
 			if(useCustom){
+				// 使用命令行提供的facelog主机/端口号连接facelog,连接失败则抛出FaceLogConnectException异常
 				type = ConnectConfigType.CUSTOM;
-				checkArgument(type.testConnect(),"NOT CONNECT TO facelog service %s:%s",
-						type.getHost(),type.getPort());
+				if(!type.testConnect()){
+					throw new FaceLogConnectException(
+							String.format("NOT CONNECT TO facelog service %s:%d", type.getHost(),type.getPort()));
+				}
 			}else{
+				// 自动寻找 facelog,找不到会抛出FaceLogConnectException异常
 				type = ConnectConfigType.lookupRedisConnect();
 			}
+			// 联网运行模式
 			IFaceLogClient facelogClient = ClientFactory.builder()
 					.setHostAndPort(type.getHost(), type.getPort())
 					.setDecorator(RefreshTokenDecorator.makeDecoratorFunction( DeviceTokenHelper.HELPER))
 					.build(IFaceLogThriftClient.class, IFaceLogClient.class);
 			new DtalkDemo(facelogClient, type)
 					.registerHelper(DeviceTokenHelper.HELPER)
+					.onlineDeviceIfFacelogPresent()
+					.start();
+		}catch (FaceLogConnectException e) {
+			try {
+				logger.info(e.getMessage());
+				// 脱网运行模式
+				RedisConfigType redisConfigType = RedisConfigType.lookupRedisConnect();
+				new DtalkDemo(redisConfigType)
 					.initDevice()
 					.start();
-			// 如果依赖库commons-pool的版本号为2.4.2则需要调用waitquit()
-			// 如果版本号高于2.4.2低于2.6.1则不需要调用，参见https://blog.csdn.net/10km/article/details/89016301
-			waitquit();
+			} catch (DtalkException de) {
+				logger.error(de.getMessage());
+//				de.printStackTrace();
+				return ;
+			}
 		}catch (Exception e) {
-			System.out.println(e.getMessage());
+			logger.error(e.getMessage());
 //			e.printStackTrace();
 			return ;
 		}
+		// 如果依赖库commons-pool的版本号为2.4.2则需要调用waitquit()
+		// 如果版本号高于2.4.2低于2.6.1则不需要调用，参见https://blog.csdn.net/10km/article/details/89016301
+		waitquit();
 	}
 
 }
