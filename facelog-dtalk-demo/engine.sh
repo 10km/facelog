fcc0730e07bbb9825b3c0cef2a337f90b1b0b661
10km <10km0811@sohu.com>
2019-06-04 15:20:23 +0800

    commit

--- facelog-dtalk-demo/engine.sh
+++ facelog-dtalk-demo/engine.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sh_folder=$(dirname $(readlink -f $0))
 pushd $sh_folder
-target\\$(basename $0) $*
+target/$(basename $0) $*
 popd
--- shade-package.sh
+++ shade-package.sh
@@ -1,1 +1,3 @@
 mvn install && mvn package -Pshade-package
+chmod +x facelog-service/target/*.sh
+chmod +x facelog-dtalk-demo/target/*.sh
