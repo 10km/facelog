9f058cddbfaa23a83b80cf585780df6396838cdd
10km <10km0811@sohu.com>
2019-06-20 18:26:10 +0800

    增加cpp/c client代码生成脚本及生成的代码

---
+++ facelog-service/gen-client-cpp.bat
@@ -0,0 +1,24 @@
+echo off
+rem 生成 FaceApi cpp client代码脚本(cpp)
+set sh_folder=%~dp0
+rem 删除最后的 '\'
+set sh_folder=%sh_folder:~0,-1%
+pushd "%sh_folder%"
+set OUT_FOLDER=%sh_folder%\..\facelog-client-ext\cpp
+:: 指定thrift compiler位置
+where thrift >nul 2>nul
+if errorlevel 1 (
+	echo thrift NOT FOUND.
+	exit /B -1
+)
+set THRIFT_EXE=thrift
+if exist "%OUT_FOLDER%" (
+	del  "%OUT_FOLDER%"\*.cpp >nul 2>nul
+	)
+if not exist "%OUT_FOLDER%" mkdir  "%OUT_FOLDER%"
+
+%THRIFT_EXE% --gen cpp:no_skeleton,moveable_types,templates^
+	-out "%OUT_FOLDER%" ^
+	%sh_folder%\IFaceLog.thrift 
+
+popd
---
+++ facelog-service/gen-client-ext.bat
@@ -0,0 +1,6 @@
+echo off
+call gen-client-c.bat
+call gen-client-cpp.bat
+call gen-client-csharp.bat
+call gen-client-jquery.bat
+call gen-client-node.js.bat
