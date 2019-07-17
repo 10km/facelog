5e7d8ee3ebed27b834063751c0690f99fcd7ec7a
10km <10km0811@sohu.com>
2019-07-17 13:07:01 +0800

    commit

--- facelog-service/shell.template/start_facelog_server.sh
+++ facelog-service/shell.template/start_facelog_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sh_folder=$(dirname $(readlink -f $0))
-pushd $sh_folder
+pushd $sh_folder > /dev/null 2>&1
 java $FACELOG_OPTS $FACELOG_DEBUG_OPTS -jar ${project.build.finalName}-standalone.jar $*
-popd
+popd  > /dev/null 2>&1
